3c37f9d4ad6db96b65c9dcbe627496699a09274e
Alexander Hmel <can4ik22@gmail.com>
2023-11-26 23:49:59 +0200

    fix: routes

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 5001
+const express = require("express");
+const app = express();
+const port = 5001;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json());
+
+app.get("/test", (req, res) => {
+  res.send({
+    name: "Mashka",
+    surname: "Gulenko",
+    login: "marikotoo",
+    password: "0:37",
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+  console.log(`Example app listening on port ${port}`);
+});
